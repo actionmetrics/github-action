883ce082c636323e028ca5957a415409969a525f
Jonathan Fielding <j.fielding@me.com>
2022-01-25 20:03:24 +0000

    test using action

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,9 @@
 import { getInput, setFailed } from "@actions/core";
 import { getOctokit } from "@actions/github";
+import pathname from "path";
+import unzip from "adm-zip";
+import filesize from "filesize";
+import fs from "fs";
 
 async function main() {
   try {
@@ -10,7 +14,7 @@ async function main() {
     const name = getInput("name");
     const branch = getInput("default_branch");
 
-    let runID;
+    let runId;
 
     const client = getOctokit(token);
 
@@ -24,14 +28,11 @@ async function main() {
       }
     )) {
       for (const run of runs.data) {
-        console.log(run.id);
         let artifacts = await client.rest.actions.listWorkflowRunArtifacts({
           owner: owner,
           repo: repo,
           run_id: run.id,
         });
-
-        console.log(artifacts);
 
         if (artifacts.data.artifacts.length == 0) {
           continue;
@@ -45,20 +46,61 @@ async function main() {
           continue;
         }
 
-        runID = run.id;
+        runId = run.id;
         break;
       }
 
-      if (runID) {
+      if (runId) {
         break;
       }
     }
 
-    if (runID) {
-      console.log("==> RunID:", runID);
+    if (runId) {
+      console.log(`*** Found run ***: ${runId}`);
     } else {
-      throw new Error("no matching workflow run found");
+      throw new Error("*** no matching workflow run found ***");
     }
+
+    let artifacts = await client.paginate(
+      client.actions.listWorkflowRunArtifacts,
+      {
+        owner: owner,
+        repo: repo,
+        run_id: runID,
+      }
+    );
+
+    const [artifact] = artifacts.filter((artifact) => {
+      return artifact.name == name;
+    });
+
+    console.log("==> Artifact:", artifact.id);
+
+    const size = filesize(artifact.size_in_bytes, { base: 10 });
+
+    console.log(`==> Downloading: ${artifact.name}.zip (${size})`);
+
+    const zip = await client.actions.downloadArtifact({
+      owner: owner,
+      repo: repo,
+      artifact_id: artifact.id,
+      archive_format: "zip",
+    });
+
+    const dir = name ? path : pathname.join(path, artifact.name);
+
+    fs.mkdirSync(dir, { recursive: true });
+
+    const adm = new unzip(Buffer.from(zip.data));
+
+    adm.getEntries().forEach((entry) => {
+      const action = entry.isDirectory ? "creating" : "inflating";
+      const filepath = pathname.join(dir, entry.entryName);
+
+      console.log(`  ${action}: ${filepath}`);
+    });
+
+    adm.extractAllTo(dir, true);
   } catch (error) {
     setFailed(error);
   }
